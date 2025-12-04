6b6fae7029664be3b699f63c7846630117ac95e1
Kaba <Kaba8081@gmail.com>
2025-12-04 09:14:12 +0100

    Feat. Check and install available dependencies for plugins when loading

--- src/sk_aio/core/loader.py
+++ src/sk_aio/core/loader.py
@@ -24,6 +24,97 @@ class PluginLoader:
     ) -> None:
         self._lodaed_plugins: Set[Plugin] = set()
 
+    @staticmethod
+    def resolve_dependencies(
+        config: tuple[Path, ppp.PyProject],
+    ) -> None:
+        if config[1].dependency_groups is None or config[1].dependency_groups['external'] is None:
+            return # no external dependencies to install
+
+        # Ignore DependencyGroupDict objects for now
+        already_installed_packages = {dist.metadata['Name'].lower() for dist in distributions() if 'Name' in dist.metadata}
+        external_packages = [
+            pkg for pkg in config[1].dependency_groups['external']
+            if isinstance(pkg, str) and pkg.lower().split('==')[0] not in already_installed_packages
+        ]
+
+        if len(external_packages) == 0:
+            return
+        logging.getLogger(__name__).debug(
+            "Installing \"%s\" for %s plugin",
+            " ".join(external_packages),
+            config[1].project['name']
+        )
+
+        (status, output) = subprocess.getstatusoutput(
+            [sys.executable, "-m", "pip", "install"] + external_packages
+        )
+        if status != 0:
+            raise RuntimeError(output)
+        logging.getLogger(__name__).debug(output)
+
+        return
+
+    @staticmethod
+    def resolve_plugin_dependencies(
+        config: tuple[Path, ppp.PyProject],
+    ) -> bool:
+        # Returns True if plugin can safely load (all deps loaded)
+        if config[1].dependency_groups is None or config[1].dependency_groups['plugins'] is None:
+            return True # no plugins dependencies
+
+        active_plugins = PluginLoader.get_active_plugins()
+        active_plugins = [ap.split('.')[-1] for ap in active_plugins]
+
+        for dep in config[1].dependency_groups['plugins']:
+            if dep not in active_plugins:
+                return False
+
+        return True
+
+    @staticmethod
+    def get_active_plugins() -> set[str]:
+        result: set[str] = set()
+
+        for name in sys.modules:
+            if "sk_aio.plugins." in name:
+                if name.find('.actions') == -1:
+                    result.add(name)
+
+        return result
+
+    @property
+    def loaded_plugins(self) -> Set[Plugin]:
+        return self._lodaed_plugins
+
+    @property
+    def loaded_plugin_count(self) -> int:
+        return len(self._lodaed_plugins)
+
+    def load_plugins(
+        self,
+        plugin_root_path: Path,
+    ) -> Set[Plugin]:
+
+        configs = self._load_plugin_configs(plugin_root_path)
+        configs = self._sort_load_order(configs)
+
+        # TODO: Add a setting to auto install dependencies or skip loading those plugins
+        # Resolve external dependencies
+        for config in configs:
+            try:
+                PluginLoader.resolve_dependencies(config)
+            except RuntimeError as error:
+                logging.getLogger(__name__).error(
+                    "Error occured while trying to install dependencies for \"%s\" plugin: %s", 
+                    config[1].project['name'],
+                    error
+                )
+
+        self._lodaed_plugins = self._load_plugins(configs)
+
+        return self.loaded_plugins
+
     def _load_pyproject(
         self,
         path: Path,
@@ -153,6 +244,14 @@ class PluginLoader:
         for config in plugin_configs:
             plugin_instance: Plugin | None = None
 
+            if not PluginLoader.resolve_plugin_dependencies(config):
+                logging.getLogger(__name__).error(
+                    "Required plugin dependencies for \"%s\" are missing. Skipping plugin. \"%s\"",
+                    config[1].project['name'],
+                    config[1].dependency_groups['plugins']
+                )
+                plugin_configs.remove(config)
+
             if config[0].is_dir():
                 plugin_instance = self._load_plugin_from_package(config[0])
 
@@ -161,65 +260,3 @@ class PluginLoader:
             plugins.add(plugin_instance)
 
         return plugins
-
-    @staticmethod
-    def resolve_dependencies(
-        config: tuple[Path, ppp.PyProject],
-    ) -> None:
-        if config[1].dependency_groups is None or config[1].dependency_groups['external'] is None:
-            return # no external dependencies to install
-
-        # Ignore DependencyGroupDict objects for now
-        already_installed_packages = {dist.metadata['Name'].lower() for dist in distributions() if 'Name' in dist.metadata}
-        external_packages = [
-            pkg for pkg in config[1].dependency_groups['external']
-            if isinstance(pkg, str) and pkg.lower().split('==')[0] not in already_installed_packages
-        ]
-
-        if len(external_packages) == 0:
-            return
-        logging.getLogger(__name__).debug(
-            "Installing \"%s\" for %s plugin",
-            " ".join(external_packages),
-            config[1].project['name']
-        )
-
-        (status, output) = subprocess.getstatusoutput(
-            [sys.executable, "-m", "pip", "install"] + external_packages
-        )
-        if status != 0:
-            raise RuntimeError(output)
-        logging.getLogger(__name__).debug(output)
-
-        return
-
-    def load_plugins(
-        self,
-        plugin_root_path: Path,
-    ) -> Set[Plugin]:
-
-        configs = self._load_plugin_configs(plugin_root_path)
-        configs = self._sort_load_order(configs)
-
-        # TODO: Add a setting to auto install dependencies or skip loading those plugins
-        for config in configs:
-            try:
-                PluginLoader.resolve_dependencies(config)
-            except RuntimeError as error:
-                logging.getLogger(__name__).error(
-                    "Error occured while trying to install dependencies for \"%s\" plugin: %s", 
-                    config[1].project['name'],
-                    error
-                )
-
-        self._lodaed_plugins = self._load_plugins(configs)
-
-        return self.loaded_plugins
-
-    @property
-    def loaded_plugins(self) -> Set[Plugin]:
-        return self._lodaed_plugins
-
-    @property
-    def loaded_plugin_count(self) -> int:
-        return len(self._lodaed_plugins)
